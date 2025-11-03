fbbfccc2842d286470b4e719c2739a18191946f4
Mason Remaley <mason@anthropicstudios.com>
2025-11-03 01:40:06 -0800

    Adds a 3D transform to ext

--- src/ext.zig
+++ src/ext.zig
@@ -2,10 +2,13 @@
 
 const std = @import("std");
 
+const Transform = @import("ext/transform.zig").Transform;
+
 pub const Node = @import("ext/Node.zig");
-pub const Transform2D = @import("ext/Transform2D.zig");
 pub const ZoneCmd = @import("ext/ZoneCmd.zig");
 pub const Tag = @import("ext/tag.zig").Tag;
+pub const Transform2D = Transform(.@"2");
+pub const Transform3D = Transform(.@"3");
 pub const geom = @import("geom");
 
 test {
